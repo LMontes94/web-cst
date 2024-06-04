be8f86cee60a8894fb0fd63742b96ae35d7de6b5
LMontes94 <monteslucas1994@gmail.com>
2024-06-04 00:03:31 -0300

    modificando componnets y layouts

--- cst/resources/views/layouts/app.blade.php
+++ cst/resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Sistema de gestión :: www.colegiosteresita.edu.ar') }}</title>
+   <!--  <title>{{ config('app.name', 'Sistema de gestión :: www.colegiosteresita.edu.ar') }}</title>-->
+    <title>Sistema de gestión :: www.colegiosteresita.edu.ar</title>
     <link rel="icon" href="{{asset('assets/img/escudo/Escudo-CST-sinFondo.ico')}}" type="image/x-icon">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -25,11 +26,11 @@
     <div class="min-h-screen bg-gray-100">
         @livewire('navigation-menu')
 
-        <!-- Page Heading -->
+        <!--  Page Heading-->
         @if (isset($header))
         <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+               {{ $header }} 
             </div>
         </header>
         @endif
